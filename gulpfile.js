e4f16186adc39a9640d5ad5bb8f3bfc39cd5b552
chenmuen <593827475@qq.com>
2017-02-23 19:53:42 +0800

    Update components

--- gulpfile.js
+++ gulpfile.js
@@ -1,84 +1,96 @@
 // Load Gulp
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    plugins = require('gulp-load-plugins')({
-        rename: {
-            'gulp-live-server': 'serve'
-        }
-    });
+  gutil = require('gulp-util'),
+  plugins = require('gulp-load-plugins')({
+    rename: {
+      'gulp-live-server': 'serve'
+    }
+  });
 var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
+var reload = browserSync.reload;
 
 // Start Watching: Run "gulp"
-gulp.task('default', ['watch']);
+gulp.task('default', ['build', 'browser-sync', 'watch']);
 
 // Run "gulp server"
 gulp.task('server', ['browser-sync', 'watch']);
 
+gulp.task('build', ['build-js', 'build-css', 'build-img', 'build-font']);
+
 // Minify Custom JS: Run manually with: "gulp build-js"
-gulp.task('build-js', function () {
-    return gulp.src('assets/js/*.js')
-        .pipe(plugins.jshint())
-        .pipe(plugins.jshint.reporter('jshint-stylish'))
-        .pipe(plugins.uglify({
-            output: {
-                'ascii_only': true
-            }
-        }))
-        .pipe(gulp.dest('build/js'));
+gulp.task('build-js', function() {
+  return gulp.src('assets/js/*.js')
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('jshint-stylish'))
+    .pipe(plugins.uglify({
+      output: {
+        'ascii_only': true
+      }
+    }))
+    .pipe(gulp.dest('build/js'));
 });
 
 // Less to CSS: Run manually with: "gulp build-css"
-gulp.task('build-css', function () {
-    return gulp.src('assets/less/*.less')
-        .pipe(plugins.plumber())
-        .pipe(plugins.less())
-        .on('error', function (err) {
-            gutil.log(err);
-            this.emit('end');
-        })
-        .pipe(plugins.autoprefixer({
-            browsers: [
-                    '> 1%',
-                    'last 2 versions',
-                    'firefox >= 4',
-                    'safari 7',
-                    'safari 8',
-                    'IE 8',
-                    'IE 9',
-                    'IE 10',
-                    'IE 11'
-                ],
-            cascade: false
-        }))
-        .pipe(plugins.cssmin())
-        .pipe(gulp.dest('build/css')).on('error', gutil.log);
+gulp.task('build-css', function() {
+  return gulp.src('assets/less/*.less')
+    .pipe(plugins.plumber())
+    .pipe(plugins.less())
+    .on('error', function(err) {
+      gutil.log(err);
+      this.emit('end');
+    })
+    .pipe(plugins.autoprefixer({
+      browsers: [
+        '> 1%',
+        'last 2 versions',
+        'firefox >= 4',
+        'safari 7',
+        'safari 8',
+        'IE 8',
+        'IE 9',
+        'IE 10',
+        'IE 11'
+      ],
+      cascade: false
+    }))
+    .pipe(plugins.cssmin())
+    .pipe(gulp.dest('build/css')).on('error', gutil.log);
 });
+
+gulp.task('build-img', function() {
+  return gulp.src('assets/img/*')
+    .pipe(gulp.dest('build/img')).on('error', gutil.log);
+})
+
+gulp.task('build-font', function() {
+  return gulp.src('assets/font/*')
+    .pipe(gulp.dest('build/font')).on('error', gutil.log);
+})
 
 // 静态服务器
 gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./",
-            index: "index.html"
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
 });
 
-// Default task
-gulp.task('watch', function () {
-    gulp.watch('*.html').on('change', reload);
-    gulp.watch('templates/*.html').on('change', reload);
-    gulp.watch('assets/js/*.js', ['build-js']).on('change', reload);
-    gulp.watch('assets/less/**/*.less', ['build-css']).on('change', reload);
+// Watch task
+gulp.task('watch', function() {
+  gulp.watch('*.html').on('change', reload);
+  gulp.watch('templates/*.html').on('change', reload);
+  gulp.watch('assets/js/*.js', ['build-js']).on('change', reload);
+  gulp.watch('assets/less/**/*.less', ['build-css']).on('change', reload);
 });
 
 // Folder "/" serving at http://localhost:8888
 // Should use Livereload (http://livereload.com/extensions/)
-gulp.task('serve', function () {
-    var server = plugins.serve.static('/', 8888);
-    server.start();
-    gulp.watch(['build/*'], function (file) {
-        server.notify.apply(server, [file]);
-    });
+gulp.task('serve', function() {
+  var server = plugins.serve.static('/', 8888);
+  server.start();
+  gulp.watch(['build/*'], function(file) {
+    server.notify.apply(server, [file]);
+  });
 });
